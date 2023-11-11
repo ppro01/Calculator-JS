f983c48a52f0b2c1756d1b028432959d3a8d7190
Francisco P <franckpedroam337@gmail.com>
2023-11-11 19:58:09 -0300

    design

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.addEventListener("onload", function () {
-  console.log("hello, world!");
+var input = document.querySelector(".CalculatorScreen");
+document.addEventListener("click", (e) => {
+  btn = e.target;
+  if (btn.classList.contains("op")) {
+    console.log(btn.innerText);
+  }
 });
